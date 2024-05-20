69e42fa4ccf9565ec19ecb5198e11b79df082c27
Gokai9 <tasim.dev@gmail.com>
2024-05-20 14:53:13 +0700

    change about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ const About = () => {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div>
-                
+                <p>I am a Software Engineer who like to build a new thing</p>
             </div>
         </main>
         
