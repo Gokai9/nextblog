144aa75ad4c6c6cc21774a745aab849e137e1868
Gokai9 <tasim.dev@gmail.com>
2024-05-21 16:00:09 +0700

    change page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,10 @@ const About = () => {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div>
-                <p>I am a Software Engineer who like to build a new thing</p>
+                <h2>Who me</h2>
+                <p>I ,
+
+                </p>
             </div>
         </main>
         
--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,16 @@ export default function Home() {
       <div>
         <div className="block pb-6 bg-black-800">
           <p className="mb-6 text-3xl font-semibold leading-normal text-white">
-          Beberapa orang mengatakan aku bisu, beberapa orang mengatakan aku bodoh. 
-          Tapi apapun yang mereka katakan aku ingin menjadi orang yang menggunakan otakku. 
+          Hello everyone, Welcome to my website i like to build a new stuf and learn new thing
           </p>
-          <Link href={"/about"} className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Click this if you want to know more about me</Link>
+          <Link href={"/about"} className="text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">know more about me</Link>
         </div>
         <div className="block pb-6">
           <p className="mb-6">
-            Walau telah belajar menulis sejak umur 6 tahun, tapi baru pertama kali ini mulai membuat karya tulis. 
+            I recently started to writing about programming stuf because it make me understand more what i doing,
+            i hope that i can be better at writing and explain new thing.
           </p>
-          <Link href={"/blogs"} className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read my blog</Link>
+          <Link href={"/blogs"} className="text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read my blog</Link>
         </div>
       </div>
     </main>
