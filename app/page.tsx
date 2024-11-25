d1dd1677c31e250bd422ebe0f3eec49c9e62e555
Gokai9 <tasim.dev@gmail.com>
2024-11-25 20:35:21 +0700

    add hamburge

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import Link from "next/link";
 import Me from "./componets/Me";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-export default function Home() {
+export default async function Home() {
+  // const genAI = new GoogleGenerativeAI('AIzaSyAov3fYWHNz3FijPiSEDo52rkLYAYk1hAs');
+  // const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  // const prompt = "Write a story about a magic backpack.";
+
+  // const result = await model.generateContent(prompt);
+  // console.log(result.response.text());
   return (
     <main className="flex min-h-screen flex-col items-center justify-center mt-10">
         <section className="flex flex-col items-center justify-center h-screen text-center px-4">
@@ -16,6 +24,7 @@ export default function Home() {
           Learn More About Me
         </Link>
       </section>
+
         <Me />
     </main>
   );
